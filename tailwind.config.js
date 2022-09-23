7cf8bfb04f4e13d616ea3bdfcb1be4b915d5af72
Ali chatraei <avida.chatraee2001@yahoo.com>
2022-09-23 10:35:29 +0330

    Install hero icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     "./src/screens/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/layouts/**/*.{js,ts,jsx,tsx}'
   ],
   // ...
 };
